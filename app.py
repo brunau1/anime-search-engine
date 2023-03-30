dd5efb1c5763c1e7d495de21d3f2f04df4cdcd6d
Bruno Santos <bruno.santos@u4crypto.com>
2023-03-30 06:34:52 -0300

    fix: change method name

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
 from flask import Flask, request, jsonify
-from src.ranking_tfidf import get_ranking_by_cos_similarity, load_tfidf_vectors
+from src.weight_ranking import get_cos_similarity_weight_ranking, load_tfidf_vectors
+from src.embeding_ranking import load_w2v_model, get_cos_similarity_w2v_ranking
 
 app = Flask(__name__)
 
-# Variável global que armazena os dados pré-processados
-anime_tfidf_data = load_tfidf_vectors()
-
+# pre build dos dados para a busca
+anime_tfidf_data = {} # load_tfidf_vectors()
+w2v_model = load_w2v_model()
 
 @app.route('/search', methods=['POST'])
 def search():
     payload = request.get_json()
+
+    print(request.args.keys())
+
     text = payload['text']
     ranking_count = int(payload['ranking_count'])
 
-    ranking = get_ranking_by_cos_similarity(
+    ranking = get_cos_similarity_weight_ranking(
         text, ranking_count, anime_tfidf_data)
+
+    response = {
+        'ranking': ranking
+    }
+
+    return jsonify(response)
+
+@app.route('/search2', methods=['POST'])
+def search2():
+    payload = request.get_json()
+
+    text = payload['text']
+    ranking_count = int(payload['ranking_count'])
+
+    ranking = get_cos_similarity_w2v_ranking(
+        w2v_model, text, ranking_count)
 
     response = {
         'ranking': ranking
