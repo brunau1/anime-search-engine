04757f07b5f9853b3c86bcc5e8a894845debc90a
Bruno Santos <bruno.santos@u4crypto.com>
2023-05-03 04:55:35 -0300

    feat: implement bleu evaluation

--- src/search_methods/services/ranking.py
+++ src/search_methods/services/ranking.py
@@ -1,6 +1,9 @@
 import numpy as np
+from nltk.translate.bleu_score import sentence_bleu
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.spatial import distance
+
+from services.preprocess import simple_preprocess_text, preprocess_text
 
 
 def cos_similarity_top_results(query_vector, text_vectors, names, top_k=10):
@@ -41,3 +44,32 @@ def euclidean_distance_top_results(query_vector, text_vectors, names, top_k=10):
         ranking.append([anime_title, similarity_score])
 
     return ranking
+
+
+def calculate_bleu_1_score_for_texts(titles, texts, search_phrase, search_encoded, texts_embeddings, top_k=10):
+    similarity_matrix = np.inner(search_encoded, texts_embeddings)
+
+    similar_text_indices = np.argsort(similarity_matrix)[::-1]
+
+    curr_scores = []
+    for i in range(0, top_k):
+        text_idx = similar_text_indices[i]
+
+        reference = preprocess_text(texts[text_idx])
+        candidate = preprocess_text(search_phrase)
+
+        # remove from the reference the words that are not in the candidate
+        reference = [word for word in reference if word in candidate]
+
+        reference = list(dict.fromkeys(reference))
+
+        score = sentence_bleu([reference], candidate, weights=(1, 0, 0, 0))
+
+        print(
+            f"BLEU score for '{' '.join(reference)}' and '{' '.join(candidate)}': {score}")
+
+        curr_scores.append(
+            {'idx': text_idx, 'title': titles[text_idx],
+             'cos_sim': np.float64(similarity_matrix[text_idx]), 'bleu_score': score})
+
+    return curr_scores
